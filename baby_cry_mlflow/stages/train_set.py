c81e22830ee9d44f44e7e778e80d4591387b38e9
Giulia Bianchi <gbianchi@xebia.fr>
2018-08-15 22:56:06 +0200

    make train_set.py executable by mlflow

--- baby_cry_mlflow/stages/train_set.py
+++ baby_cry_mlflow/stages/train_set.py
@@ -4,6 +4,7 @@ import argparse
 import re
 import os
 import numpy as np
+import mlflow
 
 from baby_cry_mlflow.utils import Reader
 from baby_cry_mlflow.utils.feature_engineer import FeatureEngineer
@@ -23,39 +24,41 @@ def main():
 
     # READ FILES IN SUB-FOLDERS of load_path and FEATURE ENGINEERING
 
-    # list load_path sub-folders
-    regex = re.compile(r'^[0-9]')
-    directory_list = [i for i in os.listdir(load_path) if regex.search(i)]
+    with mlflow.start_run():
 
-    # initialize empty array for features
-    X = np.empty([1, 18])
+        # list load_path sub-folders
+        regex = re.compile(r'^[0-9]')
+        directory_list = [i for i in os.listdir(load_path) if regex.search(i)]
 
-    # initialise empty array for labels
-    y = []
+        # initialize empty array for features
+        X = np.empty([1, 18])
 
-    # iteration on sub-folders
-    for directory in directory_list:
+        # initialise empty array for labels
+        y = []
 
-        # Instantiate FeatureEngineer
-        feature_engineer = FeatureEngineer(label=directory)
+        # iteration on sub-folders
+        for directory in directory_list:
 
-        file_list = os.listdir(os.path.join(load_path, directory))
+            # Instantiate FeatureEngineer
+            feature_engineer = FeatureEngineer(label=directory)
 
-        # iteration on audio files in each sub-folder
-        for audio_file in file_list:
-            file_reader = Reader(os.path.join(load_path, directory, audio_file))
-            data, sample_rate = file_reader.read_audio_file()
-            avg_features, label = feature_engineer.feature_engineer(audio_data=data)
+            file_list = os.listdir(os.path.join(load_path, directory))
 
-            X = np.concatenate((X, avg_features), axis=0)
-            y.append(label)
+            # iteration on audio files in each sub-folder
+            for audio_file in file_list:
+                file_reader = Reader(os.path.join(load_path, directory, audio_file))
+                data, sample_rate = file_reader.read_audio_file()
+                avg_features, label = feature_engineer.feature_engineer(audio_data=data)
 
-    # X.shape is (401, 18) as I'm not using indexing. First line is made of zeros and is to be removed
-    X = X[1:, :]
+                X = np.concatenate((X, avg_features), axis=0)
+                y.append(label)
 
-    # Save to numpy binary format
-    np.save(os.path.join(save_path, 'dataset.npy'), X)
-    np.save(os.path.join(save_path, 'labels.npy'), y)
+        # X.shape is (401, 18) as I'm not using indexing. First line is made of zeros and is to be removed
+        X = X[1:, :]
+
+        # Save to numpy binary format
+        np.save(os.path.join(save_path, 'dataset.npy'), X)
+        np.save(os.path.join(save_path, 'labels.npy'), y)
 
 if __name__ == '__main__':
     main()
